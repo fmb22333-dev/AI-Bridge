b6ba6eb06a67d34440110bc66b08058c4bbc5651
fmb22333-dev <fmb22333@gmail.com>
2026-09-09 02:55:52 +0800

    Migrate Runtime core package entrypoint

---
+++ runtime/src/ai_bridge/core/__init__.py
@@ -0,0 +1,5 @@
+from .workspace import Workspace, WorkspaceRegistry
+from .policy import PolicyDecision, PolicyEngine
+from .service import BridgeService
+
+__all__ = ["Workspace", "WorkspaceRegistry", "PolicyDecision", "PolicyEngine", "BridgeService"]
